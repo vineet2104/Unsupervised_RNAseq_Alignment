6c35b13ad4cbf0bf9d7ef1a6184de17513b7d475
vineet2104 <50541528+vineet2104@users.noreply.github.com>
2020-10-26 19:03:13 +0530

    Update vis_util.py

--- Utils/vis_util.py
+++ Utils/vis_util.py
@@ -3,6 +3,11 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from torch.autograd import Variable
+
+CAND_COLORS = np.array(['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f',
+                '#ff7f00','#cab2d6','#6a3d9a', '#90ee90', '#9B870C', '#2f4554',
+                '#61a0a8', '#d48265', '#c23531'])
 
 # pick the domains with enough cells to visualize
 def is_valid(domains, cutoff):
@@ -19,9 +24,9 @@ def is_valid(domains, cutoff):
 
 
 # plot pca visualization for certain domains with cell types colored
-def plot_pca_ct(representations, labels, domains, modelname, expname='scquery', cutoff=20):
-    if not os.path.exists('eval/%s/%s/pca2/'%(expname, modelname)):
-        os.mkdir('eval/%s/%s/pca2/'%(expname, modelname))
+def plot_pca_ct(representations, labels, domains, modelname, expname='scquery', cutoff=20,id_name = 'scDGN_modified'):
+    #if not os.path.exists('eval/%s/%s/pca2/'%(expname, modelname)):
+        #os.mkdir('eval/%s/%s/pca2/'%(expname, modelname))
     ndomains = np.unique(domains)
     for z_target in ndomains:
         indeces = np.where((domains==z_target))[0]
@@ -36,12 +41,12 @@ def plot_pca_ct(representations, labels, domains, modelname, expname='scquery', 
         ax.set_xlabel("X1")
         ax.set_ylabel("X2")
         # plt.show()
-        plt.savefig('eval/%s/%s/pca2/%s_%s.png'%(expname, modelname, modelname, d.accessions_set[z_target]))
+        plt.savefig('Image1'+id_name+'.png')
         plt.close()
 
 
 # plot the pca visualization for the whole dataset
-def plot_pca_all(representations, labels, domains, modelname, expname='scquery', nlabels=39, cutoff=20):
+def plot_pca_all(representations, labels, domains, modelname, expname='scquery', nlabels=39, cutoff=20,id_name = 'scDGN_modified'):
     indeces = np.arange(representations.shape[0])
     pca = PCA(n_components=2)
     pca.fit(representations[indeces])
@@ -52,20 +57,20 @@ def plot_pca_all(representations, labels, domains, modelname, expname='scquery',
     ax.scatter(X_reduced[:, 0], X_reduced[:, 1], s=5, alpha=0.6, c=CAND_COLORS[-3:][domains[indeces]])
     ax.set_xlabel("X1")
     ax.set_ylabel("X2")
-    plt.savefig('eval/%s/pca_domains_%s.png'%(expname, modelname))
+    plt.savefig('Image2'+id_name+'.png')
     
     plt.clf()
     fig, ax = plt.subplots()
     ax.scatter(X_reduced[:, 0], X_reduced[:, 1], s=5, alpha=0.6, c=CAND_COLORS[labels[indeces]])
     ax.set_xlabel("X1")
     ax.set_ylabel("X2")
-    plt.savefig('eval/%s/pca_labels_%s.png'%(expname, modelname))
+    plt.savefig('Image3'+id_name+'.png')
 
 
 # plot pca visualization for certain cell types with domains colored
-def plot_pca(representations, labels, domains, modelname, expname='scquery', nlabels=39, cutoff=20):
-    if not os.path.exists('eval/%s/%s/pca/'%(expname, modelname)):
-        os.mkdir('eval/%s/%s/pca/'%(expname, modelname))
+def plot_pca(representations, labels, domains, modelname, expname='scquery', nlabels=39, cutoff=20,id_name = 'scDGN_modified'):
+    #if not os.path.exists('eval/%s/%s/pca/'%(expname, modelname)):
+        #os.mkdir('eval/%s/%s/pca/'%(expname, modelname))
     for y_target in range(nlabels):
         indeces = np.where((labels==y_target))[0]
         if len(indeces) == 0 or not is_valid(domains[indeces], cutoff):
@@ -79,12 +84,12 @@ def plot_pca(representations, labels, domains, modelname, expname='scquery', nla
         ax.scatter(X_reduced[:, 0], X_reduced[:, 1], s=5, alpha=0.6, c=domains[indeces])
         ax.set_xlabel("X1")
         ax.set_ylabel("X2")
-        plt.savefig('eval/%s/%s/pca/%s_%s.png'%(expname, modelname, modelname, id2name[y_target]))
+        plt.savefig('Image4'+id_name+'.png')
         plt.close()
 
 
 # extract the representations from NN
-def extract_rep(t, d, scDGN=False):
+def extract_rep(t, d, scDGN=False,batch_size=16):
     representations = None
     labels = None
     domains = None
@@ -94,7 +99,7 @@ def extract_rep(t, d, scDGN=False):
     for i in range(n_iter):
         x = d._train_X[i*batch_size:(i+1)*batch_size] 
         y = d._train_y[i*batch_size:(i+1)*batch_size]
-        X = Variable(torch.cuda.FloatTensor(x))
+        X = torch.FloatTensor(x).cuda()
         if scDGN:
             z = d._train_acc[i*batch_size:(i+1)*batch_size]
             f_X = t.D(X, X, mode='eval')
