827a171b7806582b73b50819abd22a51f087e292
vineet2104 <50541528+vineet2104@users.noreply.github.com>
2020-10-26 19:02:40 +0530

    Update model_util.py

--- Utils/model_util.py
+++ Utils/model_util.py
@@ -30,9 +30,9 @@ class ClassicNN(nn.Module):
         elif mode == 'eval':
             return h2_output
 
-class scDGN(nn.Module):
+class scDGN_modified(nn.Module):
     def __init__(self, d_dim=20499, dim1=1136, dim2=100, dim_label=75, dim_domain=64):
-        super(scDGN, self).__init__()
+        super(scDGN_modified, self).__init__()
         self.dim1 = dim1
         self.dim2 = dim2
         self.d_dim = d_dim
@@ -79,6 +79,46 @@ class scDGN(nn.Module):
             domain_output2 = self.domain_classifier(reverse_feature2)
             #return class_output,domain_output1,domain_output2
             return class_output,decoder_output1,decoder_output2, domain_output1, domain_output2
+        elif mode == 'test':
+            #return decoder_output1,decoder_output2
+            return class_output
+        elif mode == 'eval':
+            return feature
+
+class scDGN(nn.Module):
+    def __init__(self, d_dim=20499, dim1=1136, dim2=100, dim_label=75, dim_domain=64):
+        super(scDGN, self).__init__()
+        self.dim1 = dim1
+        self.dim2 = dim2
+        self.d_dim = d_dim
+        self.dim_label = dim_label
+        self.dim_domain = dim_domain
+        self.feature_extractor = nn.Sequential(
+           nn.Linear(self.d_dim, self.dim1),
+           nn.Tanh(),
+           nn.Linear(self.dim1, self.dim2),
+           nn.Tanh(),
+        )
+        self.domain_classifier = nn.Sequential(
+            nn.Linear(self.dim2, self.dim_domain),         
+            nn.Tanh(),
+        )
+        self.label_classifier = nn.Sequential(
+            nn.Linear(self.dim2, self.dim_label),
+        )
+
+
+        print(self)
+    def forward(self, x1, x2=None, mode='train', alpha=1):
+        feature = self.feature_extractor(x1)
+        reverse_feature = ReverseLayerF.apply(feature, alpha)
+        class_output = self.label_classifier(feature)
+        if mode == 'train':
+            domain_output1 = self.domain_classifier(reverse_feature)
+            feature2 = self.feature_extractor(x2)
+            reverse_feature2 = ReverseLayerF.apply(feature2, alpha)
+            domain_output2 = self.domain_classifier(reverse_feature2)
+            return class_output,domain_output1,domain_output2
         elif mode == 'test':
             #return decoder_output1,decoder_output2
             return class_output
